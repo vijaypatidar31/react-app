546e884898bf52fc284f979f0e6019652fda354c
VIJAY PATIDAR <vramotiya@gmail.com>
2020-06-25 16:38:40 +0530

    Updates

---
+++ precache-manifest.b4576da0849f602e8aade49a92fd493c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a3bf0b6f1b7a5712aa40868514dfb1d2",
+    "url": "/react-app/index.html"
+  },
+  {
+    "revision": "0d7edd98fd6e02ecfab7",
+    "url": "/react-app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "faf47e8a0afa599a0917",
+    "url": "/react-app/static/js/2.29938b7c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-app/static/js/2.29938b7c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0d7edd98fd6e02ecfab7",
+    "url": "/react-app/static/js/main.7db76416.chunk.js"
+  },
+  {
+    "revision": "36421670ce162e5fdb4d",
+    "url": "/react-app/static/js/runtime-main.fb9a021b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
